4febb85fe7e133dc292089855dfd38eddd6326cc
meshuga <patrykorwat@gmail.com>
2013-06-01 19:51:06 +0200

    fixed case of multithreads

--- tests.py
+++ tests.py
@@ -3,24 +3,31 @@
 from alg2 import SiftMatcher
 
 import os, sys, subprocess
-import threading
+import threading, multiprocessing
 
-def runTestCase(group, siftFile, filename):
-	result = matcher.match(filename)
-	print group, siftFile, result
-
+def runTestCase(threadLimiter, group, siftFile, filename):
+	threadLimiter.acquire()
+	print '.'
+	try:
+		result = matcher.match(filename)
+		print group, siftFile, result
+	finally:
+		threadLimiter.release()
+		
 
 if __name__ == '__main__':
 	tests_dir = 'tests'
 	databaseDirectory = 'CB'
 	
 	matcher = SiftMatcher(databaseDirectory)
+	threadLimiter = threading.BoundedSemaphore(multiprocessing.cpu_count())
 	
 	for fileOrDir in os.listdir(tests_dir):
 		_fileOrDir = tests_dir + os.path.sep + fileOrDir
 		if os.path.isdir(_fileOrDir):
 			for siftFile in os.listdir(_fileOrDir):
 				if not siftFile.startswith('.') and not siftFile.endswith('.sift'):
-					thread = threading.Thread(target=runTestCase, args=(fileOrDir, siftFile, _fileOrDir + os.path.sep  + siftFile))
+					args = (threadLimiter, fileOrDir, siftFile, _fileOrDir + os.path.sep  + siftFile)
+					thread = threading.Thread(target=runTestCase, args=args)
 					thread.start()
 
